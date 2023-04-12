a980f2541ac4a22a9ef268d159e1b68926263245
Ivan Savelev <savely.vanek@mail.ru>
2023-04-12 17:28:04 +0300

    file

--- gg.py
+++ gg.py
@@ -11,8 +11,13 @@ def gcd(a, b):
             b = b % a
 
 
+<<<<<<< Updated upstream
     print('Наибольшее число:', a + b)
 
+=======
+    print('Наибольший общий делитель:', a + b)
+    print('a')
+>>>>>>> Stashed changes
 
 
 gcd(4782, 698)
